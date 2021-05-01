8e5cffcf38a2b0c65b3e47bfea84a71ed07cea36
michaljedro <michal.jedro@wp.pl>
2021-05-01 23:31:29 +0200

    end of carusel style

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('ok');
+const btnNext = document.querySelector('.fa-chevron-right');
+const btnPrevious = document.querySelector('.fa-chevron-left');
+
+const images = document.querySelectorAll('.navigation__chair');
+let imagesCounter = 0;
+
+const firstImage = images[0];
+console.log(firstImage);
+
+firstImage.classList.add('visible');
+
+btnNext.addEventListener('click', function(){
+    images[imagesCounter].classList.toggle('visible');
+    if(imagesCounter === images.length-1) {
+        imagesCounter = 0;
+    } else {
+        imagesCounter++;
+    }
+    images[imagesCounter].classList.toggle('visible');
+});
+
+btnPrevious.addEventListener('click', function () {
+    images[imagesCounter].classList.toggle('visible');
+    if (imagesCounter === 0) {
+        imagesCounter = images.length -1;
+    } else {
+        imagesCounter--;
+    }
+    images[imagesCounter].classList.toggle('visible');
+})
