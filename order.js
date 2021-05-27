734b7700f8b023108fd688b4630d886ef7b6933e
michaljedro <michal.jedro@wp.pl>
2021-05-27 16:12:08 +0200

    ad features

--- order.js
+++ order.js
@@ -9,3 +9,13 @@ arrBtn.forEach(function(item) {
     })
 })
 
+let val = document.querySelectorAll('.list_panel li');
+console.log(val);
+console.log(val.length);
+
+val.forEach(item => console.log(item.value));
+
+val.addEventListener('click', function() {
+    val.forEach(item => console.log(item.value));
+})
+
