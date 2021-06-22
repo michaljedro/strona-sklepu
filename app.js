f63ca0f65dd6ea05af174aeb884829c7cb3e8864
michaljedro <michal.jedro@wp.pl>
2021-06-22 14:25:04 +0200

    add style and JS

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
-// const btnNext = document.querySelector('.fa-chevron-right');
-// const btnPrevious = document.querySelector('.fa-chevron-left');
+const btnNext = document.querySelector('.fa-chevron-right');
+const btnPrevious = document.querySelector('.fa-chevron-left');
 
-// const images = document.querySelectorAll('.navigation__chair');
-// let imagesCounter = 0;
+const images = document.querySelectorAll('.slider__image--img');
+let imagesCounter = 0;
 
-// const firstImage = images[0];
-// console.log(firstImage);
+const firstImage = images[0];
 
-// firstImage.classList.add('visible');
+firstImage.classList.add('visible');
 
-// btnNext.addEventListener('click', function(){
-//     images[imagesCounter].classList.toggle('visible');
-//     if(imagesCounter === images.length-1) {
-//         imagesCounter = 0;
-//     } else {
-//         imagesCounter++;
-//     }
-//     images[imagesCounter].classList.toggle('visible');
-// });
+btnNext.addEventListener('click', function(){
+    images[imagesCounter].classList.toggle('visible');
+    if(imagesCounter === images.length-1) {
+        imagesCounter = 0;
+    } else {
+        imagesCounter++;
+    }
+    images[imagesCounter].classList.toggle('visible');
+});
 
-// btnPrevious.addEventListener('click', function () {
-//     images[imagesCounter].classList.toggle('visible');
-//     if (imagesCounter === 0) {
-//         imagesCounter = images.length -1;
-//     } else {
-//         imagesCounter--;
-//     }
-//     images[imagesCounter].classList.toggle('visible');
-// })
+btnPrevious.addEventListener('click', function () {
+    images[imagesCounter].classList.toggle('visible');
+    if (imagesCounter === 0) {
+        imagesCounter = images.length -1;
+    } else {
+        imagesCounter--;
+    }
+    images[imagesCounter].classList.toggle('visible');
+})
 
-// // on hover hide div text on images - section three
 
-// var container = document.querySelectorAll('.slider__img');
+var container = document.querySelectorAll('.image');
 
-// console.log(container);
+var imgs = document.querySelectorAll('.image__caption');
 
-// var imgs = document.querySelectorAll('.slider__img div');
+container.forEach(function(item, index) {
+    item.addEventListener('mouseover', function(e){
+        imgs[index].classList.toggle('image__caption--hidden')
+    })
+})
+container.forEach(function(item, index) {
+    item.addEventListener('mouseout', function(e){
+        imgs[index].classList.toggle('image__caption--hidden')
+    })
+})
 
-// console.log(imgs);
 
-// container.forEach(function(item, index) {
-//     item.addEventListener('mouseover', function(e){
-//         imgs[index].classList.toggle('slider__img--hidden')
-//     })
-// })
-// container.forEach(function(item, index) {
-//     item.addEventListener('mouseout', function(e){
-//         imgs[index].classList.toggle('slider__img--hidden')
-//     })
-// })
